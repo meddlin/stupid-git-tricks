fb4dfb29e57b39791dfd7eff286c686f79541dca
Darrien Rushing <drushing.dev@gmail.com>
2025-03-08 17:45:16 -0600

    landing page, updated layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
+import { Roboto_Mono } from 'next/font/google'
 
+const RobotoMono = Roboto_Mono()
 
 export default function Home() {
   return (
-    <div
-      className="flex h-screen bg-[#002734]"
-      style={{
-        backgroundImage:
-          "url(https://hebbkx1anhila5yf.public.blob.vercel-storage.com/builtt_personal_cover_without_logo-kkH3tDxwbYuPQef78SKUFE53nupNRF.png)",
-        backgroundSize: "auto",
-        backgroundRepeat: "repeat-y",
-        backgroundPosition: "top right",
-      }}
-    >
-      <h1 className="m-auto text-white">Stupid Git Tricks</h1>
-    </div>
+    <h1 className={`m-auto text-white text-4xl ${RobotoMono.className}`}>Stupid Git Tricks</h1>
+    // <div
+    //   className="flex h-screen bg-[#002734]"
+    //   style={{
+    //     backgroundImage:
+    //       "url(https://hebbkx1anhila5yf.public.blob.vercel-storage.com/builtt_personal_cover_without_logo-kkH3tDxwbYuPQef78SKUFE53nupNRF.png)",
+    //     backgroundSize: "auto",
+    //     backgroundRepeat: "repeat-y",
+    //     backgroundPosition: "top right",
+    //   }}
+    // >
+      
+    // </div>
   );
 }
