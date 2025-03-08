ce34cb4fc54cb123d68695925be295b569518e9e
Darrien Rushing <drushing.dev@gmail.com>
2025-03-08 17:46:01 -0600

    removed commented code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,5 @@ const RobotoMono = Roboto_Mono()
 export default function Home() {
   return (
     <h1 className={`m-auto text-white text-4xl ${RobotoMono.className}`}>Stupid Git Tricks</h1>
-    // <div
-    //   className="flex h-screen bg-[#002734]"
-    //   style={{
-    //     backgroundImage:
-    //       "url(https://hebbkx1anhila5yf.public.blob.vercel-storage.com/builtt_personal_cover_without_logo-kkH3tDxwbYuPQef78SKUFE53nupNRF.png)",
-    //     backgroundSize: "auto",
-    //     backgroundRepeat: "repeat-y",
-    //     backgroundPosition: "top right",
-    //   }}
-    // >
-      
-    // </div>
   );
 }
